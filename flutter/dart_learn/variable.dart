5bb0e1c0a06a679e9f685f7d67cd0a6ef9078dd1
dl <dai@turingm.io>
2026-01-29 18:13:35 +0800

    notes init

---
+++ flutter/dart_learn/variable.dart
@@ -0,0 +1,23 @@
+void main() {
+  // print('Hello, Dart!');
+
+  int a = 10;
+
+  double b = 20.5;
+
+  String c = 'Dart Programming';
+
+  bool d = true;
+
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  Map<String, int> studentGrades = {'John': 85, 'Jane': 90, 'Bob': 78};
+
+  // print('Hello, Dart!');
+  print(a);
+  print(b);
+  print(c);
+  print(d);
+  print(numbers);
+  print(studentGrades);
+}
