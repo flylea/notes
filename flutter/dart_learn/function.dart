7ab5728fc5b60f4e7a89ca02c492afd3824ec6a8
dl <dai@turingm.io>
2026-01-29 18:42:02 +0800

    first commit

---
+++ flutter/dart_learn/function.dart
@@ -0,0 +1,29 @@
+void main () {
+  
+  int add(int a,int b) {
+    return a + b;
+  }
+  
+  print(add(2,3));
+
+  // void printName(String name) {
+  //   print(name);
+  // }
+
+  // printName("John");
+
+  // void printName(String name) {
+  //   print(name);
+  // }
+
+  // printName("John");
+
+  // void printName(String name) {
+  //   print(name);
+  // }
+  
+  String greet(String name) => "Hello, $name!";
+  
+
+  print(greet("John"));
+}
